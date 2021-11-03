542efca350d0d266b05fb738e2e43d7bd168289f
tyler <tyler@cruftbusters.com>
2021-11-03 15:58:15 -0500

    refactor screen tests

--- src/types/Screen.test.tsx
+++ src/types/Screen.test.tsx
@@ -1,31 +1,33 @@
 import { NewScreen } from './Screen'
 
-test('stretch z=0 tile to fit wide screen', () => {
-  expect(
-    NewScreen(1024, 512, 0).tileToScreenEnvelope(0, 0, 0),
-  ).toStrictEqual([256, 0, 768, 512])
-})
+describe('tile to screen envelope', () => {
+  it('fits tile to wide screen', () => {
+    expect(
+      NewScreen(1024, 512, 0).tileToScreenEnvelope(0, 0, 0),
+    ).toStrictEqual([256, 0, 768, 512])
+  })
 
-test('stretch z=0 tile to fit tall screen', () => {
-  expect(
-    NewScreen(512, 1024, 0).tileToScreenEnvelope(0, 0, 0),
-  ).toStrictEqual([0, 256, 512, 768])
-})
+  it('fits tile to tall screen', () => {
+    expect(
+      NewScreen(512, 1024, 0).tileToScreenEnvelope(0, 0, 0),
+    ).toStrictEqual([0, 256, 512, 768])
+  })
 
-test('stretch z=1 tile to fit quarter of z=0', () => {
-  expect(NewScreen(4, 2, 0).tileToScreenEnvelope(0, 0, 1)).toStrictEqual([
-    1, 0, 2, 1,
-  ])
-})
+  it('fits zoomed tile to quarter of normal tile', () => {
+    expect(NewScreen(4, 2, 0).tileToScreenEnvelope(0, 0, 1)).toStrictEqual(
+      [1, 0, 2, 1],
+    )
+  })
 
-test('offset z=1 tile to fit right quarter of z=0', () => {
-  expect(NewScreen(4, 2, 0).tileToScreenEnvelope(1, 1, 1)).toStrictEqual([
-    2, 1, 3, 2,
-  ])
-})
+  it('fits zoomed tile to quarter of normal tile with offset', () => {
+    expect(NewScreen(4, 2, 0).tileToScreenEnvelope(1, 1, 1)).toStrictEqual(
+      [2, 1, 3, 2],
+    )
+  })
 
-test('fit z=1 tile to screen', () => {
-  expect(NewScreen(2, 2, 1).tileToScreenEnvelope(0, 0, 1)).toStrictEqual([
-    -1, -1, 1, 1,
-  ])
+  it('fits zoomed tile to zoomed normal tile', () => {
+    expect(NewScreen(2, 2, 1).tileToScreenEnvelope(0, 0, 1)).toStrictEqual(
+      [-1, -1, 1, 1],
+    )
+  })
 })
