f5e7110ade1ae2b3a61927acef3e6abf1dc152d2
tyler <tyler@cruftbusters.com>
2021-11-03 15:04:32 -0500

    refactor

--- src/types/Screen.tsx
+++ src/types/Screen.tsx
@@ -10,16 +10,14 @@ interface Screen {
 function NewScreen(width: number, height: number): Screen {
   return {
     tileToScreenEnvelope: function (this, x, y, z) {
+      const tileWidth = Math.min(width, height) / Math.pow(2, z)
       const marginLeft = Math.max(0, (width - height) / 2)
       const marginTop = Math.max(0, (height - width) / 2)
-      const zpow = Math.pow(2, z)
-      const tileWidth = (width - 2 * marginLeft) / zpow
-      const tileHeight = (height - 2 * marginTop) / zpow
       return [
         marginLeft + tileWidth * x,
-        marginTop + tileHeight * y,
+        marginTop + tileWidth * y,
         marginLeft + tileWidth * (x + 1),
-        marginTop + tileHeight * (y + 1),
+        marginTop + tileWidth * (y + 1),
       ]
     },
   }
