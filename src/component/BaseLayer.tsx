3004928dce0be4690269eebe3d143288aca6df36
tyler <tyler@cruftbusters.com>
2021-11-09 10:12:04 -0600

    refactor - base layer generates tile names functionally instead of imperatively

--- src/component/BaseLayer.tsx
+++ src/component/BaseLayer.tsx
@@ -17,12 +17,7 @@ export default function BaseLayer({ mapState }: BaseLayerProps) {
     .toEpsg3857Coordinate(mapState)
     .clamp()
     .toTile(z)
-  const tiles = []
-  for (let x = Math.floor(left); x < right; x++) {
-    for (let y = Math.floor(top); y < bottom; y++) {
-      tiles.push(new Tile(x, y, z))
-    }
-  }
+
   return (
     <div
       style={{
@@ -32,25 +27,47 @@ export default function BaseLayer({ mapState }: BaseLayerProps) {
         overflow: 'hidden',
       }}
     >
-      {tiles.map((tile) => {
-        const { x: left, y: top } = tile
-          .toEpsg3857Coordinate()
-          .toMapPixel(mapState)
-        const { x: right, y: bottom } = tile
-          .bottomRight()
-          .toEpsg3857Coordinate()
-          .toMapPixel(mapState)
-        const width = Math.round(right - left)
-        const height = Math.round(bottom - top)
-        return (
-          <BaseLayerTile
-            key={tile.string()}
-            tile={tile}
-            position={{ left, top }}
-            size={{ width, height }}
-          />
-        )
-      })}
+      {grid(
+        Math.floor(left),
+        Math.floor(top),
+        Math.ceil(right),
+        Math.ceil(bottom),
+      )
+        .map(([x, y]) => new Tile(x, y, z))
+        .map((tile) => {
+          const { x: left, y: top } = tile
+            .toEpsg3857Coordinate()
+            .toMapPixel(mapState)
+          const { x: right, y: bottom } = tile
+            .bottomRight()
+            .toEpsg3857Coordinate()
+            .toMapPixel(mapState)
+          const width = Math.round(right - left)
+          const height = Math.round(bottom - top)
+          return (
+            <BaseLayerTile
+              key={tile.string()}
+              tile={tile}
+              position={{ left, top }}
+              size={{ width, height }}
+            />
+          )
+        })}
     </div>
   )
+}
+
+function grid(
+  xStart: number,
+  yStart: number,
+  xEnd: number,
+  yEnd: number,
+): number[][] {
+  return Array.apply(null, Array(Math.ceil(xEnd) - Math.floor(xStart)))
+    .map((_, dx) =>
+      Array.apply(null, Array(Math.ceil(yEnd) - Math.floor(yStart))).map(
+        (_, dy) => [xStart + dx, yStart + dy],
+      ),
+    )
+    .flat()
 }
