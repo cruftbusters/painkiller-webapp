b90868bc373298bd7b773c0ef244a48b91daa722
tyler <tyler@cruftbusters.com>
2021-11-19 03:44:15 -0600

    disable rendering at high scale

--- src/component/Sidebar.tsx
+++ src/component/Sidebar.tsx
@@ -14,6 +14,7 @@ export default function Sidebar({
   mapState,
   onCreateMap,
 }: SidebarProps) {
+  const highScale = mapState.scale < 7
   return (
     <div
       style={{
@@ -25,6 +26,7 @@ export default function Sidebar({
     >
       <button
         style={{ fontSize: '1em' }}
+        disabled={highScale}
         onClick={async () => {
           const { width, height, left, top } = mapState
           const { x: right, y: bottom } = new MapPixel(
@@ -47,6 +49,9 @@ export default function Sidebar({
       >
         Generate heightmap
       </button>
+      {highScale ? (
+        <p>Zoom in further to enable layer rendering</p>
+      ) : undefined}
       <LayoutSummary layout={layout} />
     </div>
   )
