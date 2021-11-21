f4f6969081a1599dea28a180c739c22a0fa7dbfd
tyler <tyler@cruftbusters.com>
2021-11-21 15:44:13 -0600

    generate button summarizes errors

--- src/component/GenerateButton.tsx
+++ src/component/GenerateButton.tsx
@@ -1,6 +1,7 @@
 import MapPixel from '../types/MapPixel'
 import MapState from '../types/MapState'
 import Layout from '../types/Layout'
+import { useState } from 'react'
 
 interface GenerateButtonProps {
   mapState: MapState
@@ -12,12 +13,15 @@ export default function GenerateButton({
   onCreateMap,
 }: GenerateButtonProps) {
   const highScale = mapState.scale < 7
+  const [error, setError] = useState('')
   return (
     <>
       <button
         style={{ fontSize: '1em' }}
         disabled={highScale}
         onClick={async () => {
+          setError('')
+
           const { width, height, left, top } = mapState
           const { x: right, y: bottom } = new MapPixel(
             width,
@@ -33,12 +37,25 @@ export default function GenerateButton({
               }),
             },
           )
-
-          onCreateMap(await response.json())
+          if (response.status < 200 || response.status >= 300) {
+            setError(
+              `got status code ${
+                response.statusText
+              }: ${await response.text()}`,
+            )
+          } else {
+            onCreateMap(await response.json())
+          }
         }}
       >
         Generate layers
       </button>
+      {error ? (
+        <>
+          <p>There was an error generating layers. Try again later.</p>
+          <p style={{ color: 'gray' }}>Error: {error}</p>
+        </>
+      ) : undefined}
       {highScale ? (
         <p>Zoom in further to enable layer rendering</p>
       ) : undefined}
